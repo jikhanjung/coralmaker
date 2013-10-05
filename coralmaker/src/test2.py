43918167e2eced273463a17bfb57ae790340f181
jikhanjung <honestjung@gmail.com>
2013-10-05 03:42:44 -0500

    rotate and get_angle_between added

--- coralmaker/src/test2.py
+++ coralmaker/src/test2.py
@@ -11,6 +11,27 @@ ATTENUATION_COEFF = 4.6 / 50.0
 SURFACE_IRRADIANCE = 1361
 MAX_IRRADIANCE = 100
 REFLECTION_RATE = 0.05
+Y_INDEX = 1
+def rotate(vec,theta):
+    rotation_matrix = matrix( [ [ math.cos( theta ), 0, math.sin( theta )] , [-1 * math.sin(theta ), 1, math.cos(theta) ], [0,0,1]])
+    print vec
+    print rotation_matrix
+    new_vec = dot( vec, rotation_matrix )
+    return new_vec
+
+def get_angle_between_vectors( v1, v2 ):
+    
+    cross_product = cross( v1, v2 )
+    sin_theta = cross_product[Y_INDEX] / ( linalg.norm( v1 ) * linalg.norm( v2 ) )
+    print sin_theta
+    if sin_theta > 0:
+        print "plus"
+        
+    else:
+        print "minus"
+    theta = math.asin( sin_theta )
+    return theta
+
 
 def get_irradiance( depth, grow_vector ):
     vec = grow_vector / linalg.norm( grow_vector ) 
@@ -55,11 +76,17 @@ p1 = array( [ -5, 0, 2.3 ], float )
 p2 = array( [ 0, 0, 3 ], float )
 v1 = array( [ -3, 0, 4 ], float )
 v2 = array( [ 0, 0, 1 ], float)
+
 print p1, p2
 l = find_bud_loc_2d( v1, v2, p1, p2 )
 print l 
 
+print "angle"
+print v1, v2
+t = get_angle_between_vectors( v1, v2 )
 
+v3 = rotate( v1, t )
+print v3
 if False:
     DEPTH = [ 1, 20, 50 ]
     vec = [ array( [ 0, 0, 1 ], float), array( [ 1, 0, 1 ], float), array( [ 1, 0, 0 ], float) ] 
